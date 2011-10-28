d4425eb568def1e5b56dd484c310391af08405bb
yuko lin <yuhua829@gmail.com>
2011-10-28 10:00:09 +0800

    add sumo

---
+++ ChickenFight2011/sumoGameLayer.h
@@ -0,0 +1,16 @@
+//
+//  sumoGameLayer.h
+//  ChickenFight2011
+//
+//  Created by Lozen on 11/10/28.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface sumoGameLayer : CCLayer {
+    
+}
+
+@end
---
+++ ChickenFight2011/sumoGameLayer.m
@@ -0,0 +1,14 @@
+//
+//  sumoGameLayer.m
+//  ChickenFight2011
+//
+//  Created by Lozen on 11/10/28.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import "sumoGameLayer.h"
+
+
+@implementation sumoGameLayer
+
+@end
